845151a93a68d47d842a967dfb9c328c60e8039f
Dev AKS <devaks24k@gmail.com>
2025-02-23 17:50:55 -0500

    refactor: useOpenAI hooks

--- src/hooks/use-open-ai.ts
+++ src/hooks/use-open-ai.ts
@@ -1,45 +1,45 @@
 import OpenAI from "openai";
+
 let openai: OpenAI | null = null;
+const API_KEY = import.meta.env.VITE_OPENAI_API_KEY;
+const BASE_URL = import.meta.env.VITE_OPENAI_API_URL;
+const LLM_MODEL = "deepseek/deepseek-r1-distill-llama-70b:free";
 
 export const useOpenAI = () => {
+  if (!API_KEY) {
+    throw new Error("OpenAI API key not found");
+  }
 
-    const initializeOpenAI = async () => {
-      if (openai) {
-          return openai;
-      }
-      const apiKey = import.meta.env.OPENAI_API_KEY;
-      if (!apiKey) {
-          throw new Error("OpenAI API key not found");
-      }
-      
-      openai = new OpenAI({
-        baseURL: import.meta.env.OPENAI_API_URL,
-        apiKey,
-      });
-    };
+  const initializeOpenAI = async () => {
+    if (openai) return openai;
 
-    const getCompletion = async (prompt: string) => {
-      if (!openai) {
-        throw new Error("OpenAI is not initialized");
-      }
-      
-      const completion = await openai.chat.completions.create({
-        model: 'openai/gpt-4o',
-        messages: [
-          {
-            role: 'user',
-            content: prompt,
-          },
-        ],
-      });
+    openai = new OpenAI({
+      dangerouslyAllowBrowser: true,
+      baseURL: BASE_URL,
+      apiKey: API_KEY,
+    });
+  };
 
-      console.log({ completion });
+  const getCompletion = async (prompt: string) => {
+    if (!openai) throw new Error("OpenAI is not initialized");
+    
+    const completion = await openai.chat.completions.create({
+      model: LLM_MODEL,
+      messages: [
+        {
+          role: 'user',
+          content: prompt,
+        },
+      ],
+    });
 
-      return completion.choices[0].message;
-    };
+    console.log({ completion });
 
-    return {
-      initializeOpenAI,
-      getCompletion,
-    };
+    return completion.choices[0].message.content;
+  };
+
+  return {
+    initializeOpenAI,
+    getCompletion,
+  };
 };
