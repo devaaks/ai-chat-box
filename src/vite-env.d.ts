1aa0048a57a728016ec4bae2e26453e07a929a1b
Dev AKS <devaks24k@gmail.com>
2025-02-23 18:58:39 -0500

    feat: add chat feature

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_OPENAI_API_URL: string;
+  readonly VITE_OPENAI_API_KEY: string;
+  // add more vars as needed...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
